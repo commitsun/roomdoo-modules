233d53f08a8239cf6a89665b16c84f06c1b3065e
jesusvmayor <jesus@roomdoo.com>
2025-07-07 15:35:45 +0200

    [ADD]roomdoo_fastapi: Added module.

---
+++ roomdoo_fastapi/routers/instance.py
@@ -0,0 +1,27 @@
+
+from typing import Annotated
+from odoo.addons.pms_fastapi.models.fastapi_endpoint import pms_api_router
+from odoo.addons.roomdoo_fastapi.schemas.instance import Instance
+from odoo.api import Environment
+from odoo.addons.fastapi.dependencies import odoo_env
+from fastapi import Depends
+
+
+@pms_api_router.get(
+    '/instance/',
+    response_model=Instance,
+    tags=["db_info"]
+)
+async def get_instance_info(env: Annotated[Environment, Depends(odoo_env)])  -> Instance:
+    """
+        Get instance name and image URL.
+    """
+    instance_name = env['ir.config_parameter'].sudo().get_param('roomdoo_fastapi.instance_name', default='Roomdoo')
+    instance_image = env['ir.config_parameter'].sudo().get_param('roomdoo_fastapi.instance_image')
+    if not instance_image:
+        web_base_url = env['ir.config_parameter'].sudo().get_param('web.base.url')
+        instance_image = f'{web_base_url}/web/binary/company_logo'
+    return  Instance(
+        name=instance_name,
+        image=instance_image
+        )
