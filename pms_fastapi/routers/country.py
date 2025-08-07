aa2f862f82a29acf62a8ea1657d79e20306a1e30
jesusvmayor <jesus@roomdoo.com>
2025-08-07 15:57:46 +0200

    [IMP]pms_fastapi: Added country code in /countries endpoint.

--- pms_fastapi/routers/country.py
+++ pms_fastapi/routers/country.py
@@ -6,15 +6,15 @@ from odoo.api import Environment
 
 from odoo.addons.fastapi.dependencies import odoo_env
 from odoo.addons.pms_fastapi.models.fastapi_endpoint import pms_api_router
-from odoo.addons.pms_fastapi.schemas.country import CountryId
+from odoo.addons.pms_fastapi.schemas.country import CountrySummary
 
 
-@pms_api_router.get("/countries", response_model=list[CountryId], tags=["db_info"])
+@pms_api_router.get("/countries", response_model=list[CountrySummary], tags=["db_info"])
 async def get_server_countries(
     env: Annotated[Environment, Depends(odoo_env)]
-) -> list[CountryId]:
+) -> list[CountrySummary]:
     """
     Get countries configured in the instance.
     """
     countries = env["res.country"].sudo().search([])
-    return [CountryId.from_res_country(country) for country in countries]
+    return [CountrySummary.from_res_country(country) for country in countries]
