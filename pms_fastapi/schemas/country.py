880b5ef937f0520888420d5068b1c14f3992f3d6
jesusvmayor <jesus@roomdoo.com>
2025-08-07 15:57:46 +0200

    [IMP]pms_fastapi: Added country code in /countries endpoint.

--- pms_fastapi/schemas/country.py
+++ pms_fastapi/schemas/country.py
@@ -6,5 +6,23 @@ class CountryId(PmsBaseModel):
     name: str
 
     @classmethod
+    def parse_common_fields(cls, country) -> dict:
+        return {
+            "id": country.id,
+            "name": country.name,
+        }
+
+    @classmethod
     def from_res_country(cls, country):
-        return cls(id=country.id, name=country.name)
+        data = cls.parse_common_fields(country)
+        return cls(**data)
+
+
+class CountrySummary(CountryId):
+    code: str
+
+    @classmethod
+    def from_res_country(cls, country):
+        data = cls.parse_common_fields(country)
+        data["code"] = country.code
+        return cls(**data)
