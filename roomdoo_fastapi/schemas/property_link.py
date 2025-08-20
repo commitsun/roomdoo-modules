bd822261f013032283aab4d722aae9acd569a16c
jesusvmayor <jesus@roomdoo.com>
2025-08-20 12:00:54 +0200

    [IMP]pms_fastapi: Added property image and naming improvements.
    Renamed /properties endpoints to /pmsProperties
    Renamed support_link field to isSupportLink
    Renamed defaultProperty to defaultPmsProperty in user endpoint.
    Added property image in /pmsProperties endpoint

--- roomdoo_fastapi/schemas/property_link.py
+++ roomdoo_fastapi/schemas/property_link.py
@@ -4,8 +4,8 @@ from odoo.addons.pms_fastapi.schemas.base import PmsBaseModel
 class PropertyLink(PmsBaseModel):
     id: int
     label: str
-    support_link: bool
+    isSupportLink: bool
 
     @classmethod
     def from_pms_property_menu(cls, menu):
-        return PropertyLink(id=menu.id, label=menu.name, support_link=menu.support_url)
+        return PropertyLink(id=menu.id, label=menu.name, isSupportLink=menu.support_url)
