674dd7be2983c229cc76afc216c2da0f527eca3b
jesusvmayor <jesus@roomdoo.com>
2025-09-08 10:37:53 +0200

    [FIX]pms_fastapi: pms properties endpoints renamed to kebab case.

--- roomdoo_fastapi/routers/pms_property_url.py
+++ roomdoo_fastapi/routers/pms_property_url.py
@@ -11,7 +11,7 @@ from odoo.addons.roomdoo_fastapi.schemas.property_link import PropertyLink
 
 
 @pms_api_router.get(
-    "/pmsProperties/{property_id}/links",
+    "/pms-properties/{property_id}/links",
     status_code=200,
     responses={
         200: {"model": None},
@@ -37,7 +37,7 @@ async def get_property_links(
 
 
 @pms_api_router.get(
-    "/pmsProperties/{property_id}/links/{link_id}",
+    "/pms-properties/{property_id}/links/{link_id}",
     status_code=200,
     responses={
         404: {
--- roomdoo_fastapi/tests/test_pms_property_urls.py
+++ roomdoo_fastapi/tests/test_pms_property_urls.py
@@ -14,14 +14,14 @@ class TestLinksEndpoints(CommonTestRoomdooApi):
     def test_get_links(self):
         with self._create_test_client() as test_client:
             response = self._login(test_client)
-            response = test_client.get(f"/pmsProperties/{self.test_property.id}/links")
+            response = test_client.get(f"/pms-properties/{self.test_property.id}/links")
             self.assertEqual(response.status_code, status.HTTP_200_OK, response.text)
 
     def test_render_link(self):
         with self._create_test_client() as test_client:
             response = self._login(test_client)
             response = test_client.get(
-                f"/pmsProperties/{self.test_property.id}/links/{self.test_link.id}"
+                f"/pms-properties/{self.test_property.id}/links/{self.test_link.id}"
             )
             self.assertEqual(response.status_code, status.HTTP_200_OK, response.text)
             self.assertEqual(response.json(), self.test_link.base_url)
