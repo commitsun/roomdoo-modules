a571e7cb67c00b585a8721c094ce1d384d991771
jesusvmayor <jesus@roomdoo.com>
2025-09-08 12:57:59 +0200

    [FIX]pms_api_rest: Remove outdated fields and fix error when check_credentials fails.

--- pms_api_rest/controllers/login.py
+++ pms_api_rest/controllers/login.py
@@ -1,12 +1,9 @@
-
-import json
-from odoo import http, _, SUPERUSER_ID
+from odoo import SUPERUSER_ID, http
 from odoo.exceptions import AccessDenied
 from odoo.http import request
 
 
 class LoginPortalToken(http.Controller):
-
     @http.route(
         ["/login_portal_token"],
         type="http",
@@ -20,23 +17,25 @@ class LoginPortalToken(http.Controller):
         :param token: Token for authentication.
         :param redirect_url: Optional URL to redirect after login.
         """
-        request.params['login'] = login
-        request.params['password'] = token
-        request.params['login_success'] = False
+        request.params["login"] = login
+        request.params["password"] = token
+        request.params["login_success"] = False
         if not request.uid:
             request.uid = SUPERUSER_ID
 
         values = request.params.copy()
         try:
-            values['databases'] = http.db_list()
+            values["databases"] = http.db_list()
         except AccessDenied:
-            values['databases'] = None
+            values["databases"] = None
 
         old_uid = request.uid
         try:
-            request.session.authenticate(request.session.db, request.params['login'], request.params['password'])
-            request.params['login_success'] = True
-            return request.redirect_query(redirect_url or '/web', query=kwargs)
+            request.session.authenticate(
+                request.session.db, request.params["login"], request.params["password"]
+            )
+            request.params["login_success"] = True
+            return request.redirect_query(redirect_url or "/web", query=kwargs)
         except AccessDenied:
-            request.uid = old_uid
-        return request.redirect('/')
+            request.update_env(user=old_uid)
+        return request.redirect("/")
