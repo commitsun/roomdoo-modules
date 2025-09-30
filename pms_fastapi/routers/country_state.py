7a6a825509cfab693e8f9ef5105acb38f386d976
jesusvmayor <jesus@roomdoo.com>
2025-09-30 17:15:04 +0200

    [IMP]pms_fastapi: Allow to filter states by country.

--- pms_fastapi/routers/country_state.py
+++ pms_fastapi/routers/country_state.py
@@ -13,12 +13,16 @@ from odoo.addons.pms_fastapi.schemas.country_state import CountryStateSummary
     "/country-states", response_model=list[CountryStateSummary], tags=["db_info"]
 )
 async def get_country_states(
-    env: Annotated[Environment, Depends(odoo_env)]
+    env: Annotated[Environment, Depends(odoo_env)],
+    country: int | None = None,
 ) -> list[CountryStateSummary]:
     """
     Get country states configured in the instance.
     """
-    country_states = env["res.country.state"].sudo().search([])
+    country_state_search = []
+    if country:
+        country_state_search.append(("country_id", "=", country))
+    country_states = env["res.country.state"].sudo().search(country_state_search)
     return [
         CountryStateSummary.from_res_country_state(country_state)
         for country_state in country_states
