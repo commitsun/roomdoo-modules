b91ee0c68a8802b8cded472598b7ed9a0a9bd398
jesusvmayor <jesus@roomdoo.com>
2025-10-08 13:35:10 +0200

    Add lastname2 in user patch.

--- pms_fastapi_contact_lastname2/schemas/user.py
+++ pms_fastapi_contact_lastname2/schemas/user.py
@@ -1,11 +1,11 @@
+from pydantic import Field
+
 from odoo.addons.pms_fastapi.schemas import user
 
 
 class UserLastname2(user.User, extends=True):
-    lastname2: str = ""
+    lastname2: str = Field("", alias="lastname2")
 
-    @classmethod
-    def from_res_users(cls, user_record) -> dict:
-        res = super().from_res_users(user_record)
-        res.lastname2 = user_record.lastname2 or ""
-        return res
+
+class UserUpdate(user.UserUpdate, extends=True):
+    lastname2: str | None = Field(None, alias="lastname2")
