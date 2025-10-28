2d55dd10a40710b9c41120e0fe8b4b8cfb528286
jesusvmayor <jesus@roomdoo.com>
2025-10-28 12:12:16 +0100

    [IMP]roomdoo_fastapi: Add zip autocomplete endpoint.

---
+++ roomdoo_fastapi/schemas/zip_autocomplete.py
@@ -0,0 +1,25 @@
+from odoo.addons.pms_fastapi.schemas.base import PmsBaseModel
+from odoo.addons.pms_fastapi.schemas.country import CountryId
+from odoo.addons.pms_fastapi.schemas.country_state import CountryStateId
+
+
+class zipSummary(PmsBaseModel):
+    zip: str
+    city: str | None = None
+    state: CountryStateId | None = None
+    country: CountryId | None = None
+
+    @classmethod
+    def from_res_city_zip(cls, res_city_zip_record):
+        return zipSummary(
+            zip=res_city_zip_record.name,
+            city=res_city_zip_record.city_id.name
+            if res_city_zip_record.city_id
+            else None,
+            state=CountryStateId.from_res_country_state(res_city_zip_record.state_id)
+            if res_city_zip_record.state_id
+            else None,
+            country=CountryId.from_res_country(res_city_zip_record.country_id)
+            if res_city_zip_record.country_id
+            else None,
+        )
