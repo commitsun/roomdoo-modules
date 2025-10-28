c1506b13a67b0aa703a543a4ed79c0964eba63e2
jesusvmayor <jesus@roomdoo.com>
2025-10-28 12:12:16 +0100

    [IMP]roomdoo_fastapi: Add zip autocomplete endpoint.

---
+++ roomdoo_fastapi/routers/zip_autocomplete.py
@@ -0,0 +1,31 @@
+from typing import Annotated
+
+from fastapi import Depends
+
+from odoo.api import Environment
+
+from odoo.addons.fastapi.dependencies import odoo_env
+from odoo.addons.pms_fastapi.models.fastapi_endpoint import pms_api_router
+from odoo.addons.roomdoo_fastapi.schemas.zip_autocomplete import zipSummary
+
+
+@pms_api_router.get(
+    "/zip-autocomplete", response_model=list[zipSummary], tags=["db_info"]
+)
+async def zip_autocomplete(
+    env: Annotated[Environment, Depends(odoo_env)],
+    searchParam: str,
+) -> list[zipSummary]:
+    """
+    Get zip code autocomplete suggestions based on the search parameter.
+    Only searches with 3 or more characters will return results.
+    returns a list of max 20 results.
+    """
+    if len(searchParam) < 3:
+        return []
+    records = env["res.city.zip"].name_search(searchParam, limit=20)
+    zip_autocomplete_list = []
+    for record in records:
+        zip_record = env["res.city.zip"].browse(record[0])
+        zip_autocomplete_list.append(zipSummary.from_res_city_zip(zip_record))
+    return zip_autocomplete_list
