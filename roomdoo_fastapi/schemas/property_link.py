f2a0145bbea6bce94cd6fc947f68fe969f50fb5a
jesusvmayor <jesus@roomdoo.com>
2025-10-31 11:34:16 +0100

    [IMP]pms_api_rest: Generate report url correctly.

--- roomdoo_fastapi/schemas/property_link.py
+++ roomdoo_fastapi/schemas/property_link.py
@@ -5,7 +5,13 @@ class PropertyLink(PmsBaseModel):
     id: int
     label: str
     isSupportLink: bool
+    isReportLink: bool
 
     @classmethod
     def from_pms_property_menu(cls, menu):
-        return PropertyLink(id=menu.id, label=menu.name, isSupportLink=menu.support_url)
+        return PropertyLink(
+            id=menu.id,
+            label=menu.name,
+            isSupportLink=menu.support_url,
+            isReportLink=menu.report_url,
+        )
