b669b3575714e9e1052aa6aa3bcdea9d0b761a7c
jesusvmayor <jesus@roomdoo.com>
2025-11-10 12:06:20 +0100

    [ADD]res_partner_unique_identification: Added module.

---
+++ res_partner_unique_identification/tests/test_unique_identification.py
@@ -0,0 +1,118 @@
+from odoo.exceptions import ValidationError
+from odoo.tests import common
+
+
+class TestPms(common.TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.partner_1 = cls.env["res.partner"].create(
+            {
+                "name": "Partner 1",
+            }
+        )
+        cls.partner_2 = cls.env["res.partner"].create(
+            {
+                "name": "Partner 2",
+            }
+        )
+
+    def test_unique_vat_country_code(self):
+        """
+        Check the constraints with a partner with country code in VAT and another
+        partner trying to use the same VAT without country code but with same country.
+        """
+        self.partner_1.with_context(test_vat=True).write({"vat": "ES12345678Z"})
+        self.partner_2.write({"country_id": self.env.ref("base.es").id})
+        with self.assertRaises(ValidationError):
+            self.partner_2.with_context(test_vat=True).write({"vat": "12345678Z"})
+
+    def test_unique_vat_identification_vat_type(self):
+        """
+        Check the constraints with a partner with a country code in VAT and
+        another with a res.partner.id_number with vat type and the same number
+
+        """
+        self.partner_1.with_context(test_vat=True).write({"vat": "ES12345678Z"})
+        identification_type_vat = self.env["res.partner.id_category"].search(
+            [("aeat_identification_type", "in", ["02", "04"])], limit=1
+        )
+        with self.assertRaises(ValidationError):
+            self.env["res.partner.id_number"].create(
+                {
+                    "partner_id": self.partner_2.id,
+                    "category_id": identification_type_vat.id,
+                    "name": "12345678Z",
+                    "country_id": self.env.ref("base.es").id,
+                }
+            )
+
+    def test_unique_aeat_identification(self):
+        """
+        Check the constraints with a partner with a res.partner.id_number
+        and another with the same AEAT identification type and number.
+        """
+        identification_type_passport = self.env["res.partner.id_category"].search(
+            [("aeat_identification_type", "=", ["03"])], limit=1
+        )
+        self.env["res.partner.id_number"].create(
+            {
+                "partner_id": self.partner_1.id,
+                "category_id": identification_type_passport.id,
+                "name": "A12345678",
+                "country_id": self.env.ref("base.es").id,
+            }
+        )
+        with self.assertRaises(ValidationError):
+            self.env["res.partner.id_number"].create(
+                {
+                    "partner_id": self.partner_2.id,
+                    "category_id": identification_type_passport.id,
+                    "name": "A12345678",
+                    "country_id": self.env.ref("base.es").id,
+                }
+            )
+
+    def test_unique_aeat_fields(self):
+        """
+        Check the constraints with a partner with AEAT identification
+        and another with the same AEAT identification type and number.
+        """
+        self.partner_1.write(
+            {
+                "aeat_identification": "A12345678",
+                "aeat_identification_type": "03",
+            }
+        )
+        with self.assertRaises(ValidationError):
+            self.partner_2.write(
+                {
+                    "aeat_identification": "A12345678",
+                    "aeat_identification_type": "03",
+                }
+            )
+
+    def test_unique_aeat_identification_aeat_fields(self):
+        """
+        Check the constraints with a partner with AEAT identification
+        and another with a res.partner.id_number with the same AEAT
+        identification type and number.
+        """
+        identification_type_passport = self.env["res.partner.id_category"].search(
+            [("aeat_identification_type", "=", ["03"])], limit=1
+        )
+        self.partner_1.write(
+            {
+                "aeat_identification": "A12345678",
+                "aeat_identification_type": "03",
+            }
+        )
+        with self.assertRaises(ValidationError):
+            self.env["res.partner.id_number"].create(
+                {
+                    "partner_id": self.partner_2.id,
+                    "category_id": identification_type_passport.id,
+                    "name": "A12345678",
+                    "country_id": self.env.ref("base.es").id,
+                }
+            )
