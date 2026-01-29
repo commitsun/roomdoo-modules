54e4fe2be74368ed6ea23290f9160b147fd52a9c
jesusvmayor <jesus@roomdoo.com>
2026-01-29 13:58:25 +0100

    [FIX]pms_partner_type_residence: More robust logics in set_partner_address.
    Change the logics of the inherited method:
    
    - If partner has no residence address and the changes do not conflict with
      the partner's address: update partner address.
    - If partner has residence child: update residence with checkin values
    - If the changes conflict with partner's address:
      update residence if exists, else create residence child.

--- pms_partner_type_residence/models/pms_checkin_partner.py
+++ pms_partner_type_residence/models/pms_checkin_partner.py
@@ -4,36 +4,60 @@ from odoo import models
 class PMSCheckinPartner(models.Model):
     _inherit = "pms.checkin.partner"
 
-    def set_partner_address(self):
+    def set_partner_address(self, residence_vals=None):
         """
-        If the partner has a child of type 'residence', set the address fields
-        (street, street2, zip, city, country_id, state_id) of the check-in partner
-        to the address of that residence partner.
-        If the partner has an adress but no residence, create the residence partner
+        Sets the checkin.partner address in the associated partner
+        or its residence child.
+
+        - If partner has no residence address and the changes do not conflict with
+          the partner's address: update partner address.
+        - If partner has residence child: update residence with checkin values
+        - If the changes conflict with partner's address:
+          update residence if exists, else create residence child.
         """
-        for record in self:
+        self.ensure_one()
+        if not self.partner_id:
+            return
+
+        address_fields = {"street", "street2", "zip", "city", "country_id", "state_id"}
+        if residence_vals is None:
             residence_vals = {
-                "street": record.street,
-                "street2": record.street2,
-                "zip": record.zip,
-                "city": record.city,
-                "country_id": record.country_id.id,
-                "state_id": record.state_id.id,
+                field: self[field].id if hasattr(self[field], "id") else self[field]
+                for field in address_fields
             }
-            if any(residence_vals.values()):
-                if record.partner_id:
-                    address_fields = residence_vals.keys()
-                    if not any(record.partner_id[field] for field in address_fields):
-                        super(PMSCheckinPartner, record).set_partner_address()
-                    else:
-                        residence = record.partner_id.residence_partner_id
-                        if residence and residence != record.partner_id:
-                            residence.write(residence_vals)
-                        else:
-                            residence_vals.update(
-                                {
-                                    "parent_id": record.partner_id.id,
-                                    "type": "residence",
-                                }
-                            )
-                            self.env["res.partner"].create(residence_vals)
+
+        if not any(residence_vals.values()):
+            return
+        partner = self.partner_id
+
+        address_fields_writed = residence_vals.keys()
+        conflict_partner_address = any(
+            self.partner_id[field]
+            and self.partner_id[field] != residence_vals.get(field)
+            for field in address_fields_writed
+        )
+        residence = self.partner_id.residence_partner_id
+        if not conflict_partner_address and residence == self.partner_id:
+            return super().set_partner_address(residence_vals)
+        if residence and residence != self.partner_id:
+            residence.write(residence_vals)
+        else:
+            # Establish the base fields, to preserve existing data
+            partner_address = {
+                field: (
+                    partner[field].id
+                    if hasattr(partner[field], "id")
+                    else partner[field]
+                )
+                for field in address_fields
+            }
+            # Update with residence write values
+            partner_address.update(residence_vals)
+
+            partner_address.update(
+                {
+                    "parent_id": self.partner_id.id,
+                    "type": "residence",
+                }
+            )
+            self.env["res.partner"].create(partner_address)
