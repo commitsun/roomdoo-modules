2e32da598e510c6221abdecd7b956d906709e721
jesusvmayor <jesus@roomdoo.com>
2026-02-09 20:10:32 +0100

    [ADD]pms_fastapi_verifactu: Added module.

---
+++ pms_fastapi_verifactu/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026 Roomdoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "PMS FastAPI VERI*FACTU",
+    "version": "16.0.1.0.0",
+    "summary": "Add VERI*FACTU state in invoice endpoints",
+    "category": "Generic Modules/Property Management System",
+    "author": "Commit [Sun], Odoo Community Association (OCA)",
+    "website": "",
+    "license": "AGPL-3",
+    "depends": ["pms_fastapi", "l10n_es_verifactu_oca"],
+    "data": [],
+    "auto_install": True,
+}
