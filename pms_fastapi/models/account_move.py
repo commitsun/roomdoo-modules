0b5ac64d2276c9f5409215a3298036d3dbf5ebae
jesusvmayor <jesus@roomdoo.com>
2026-02-20 15:22:30 +0100

    [FIX]pms_fastapi: Fix paymentMethod search.

--- pms_fastapi/models/account_move.py
+++ pms_fastapi/models/account_move.py
@@ -4,6 +4,11 @@ from odoo import fields, models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
+    payment_method_ids = fields.Many2many(
+        comodel_name="account.payment.method",
+        string="Payment Methods",
+        search="_search_payment_method_ids",
+    )
     has_overdue_payments = fields.Boolean(
         string="Has Overdue Payments",
         compute="_compute_has_overdue_payments",
@@ -12,6 +17,31 @@ class AccountMove(models.Model):
     min_overdue_date = fields.Date(
         string="Minimum Overdue Payment Date", compute="_compute_min_overdue_date"
     )
+
+    def _search_payment_method_ids(self, operator, value):
+        if operator == "=":
+            value = [value]
+        elif operator != "in":
+            raise NotImplementedError(
+                f"Operator '{operator}' is not supported for payment_method_ids search."
+            )
+        payment_amls = self.env["account.move.line"].search(
+            [("payment_id.payment_method_line_id.payment_method_id", "in", value)]
+        )
+        if not payment_amls:
+            return [("id", "=", False)]
+        partials = self.env["account.partial.reconcile"].search(
+            [
+                "|",
+                ("credit_move_id", "in", payment_amls.ids),
+                ("debit_move_id", "in", payment_amls.ids),
+            ]
+        )
+        invoice_aml_ids = list(
+            (set(partials.debit_move_id.ids) | set(partials.credit_move_id.ids))
+            - set(payment_amls.ids)
+        )
+        return [("line_ids", "in", invoice_aml_ids)]
 
     def _compute_min_overdue_date(self):
         for move in self:
